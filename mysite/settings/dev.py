67f7806a6cac9c760efe338c8ab2a148af0e5b64
Genesis Guerrero Martinez <genesisdaft@gmail.com>
2024-06-01 21:32:17 +0000

    tutorial

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-))7-7akkc0i2^#o%9ppj8ma0=ggb(czl79zt=pev8!_rh4d(26"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
